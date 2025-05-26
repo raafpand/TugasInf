7c2336b870f451d9a0ac56d011779d5b80ff820f
raafpand <raafipandhika@gmail.com>
2025-05-27 06:48:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Shandi Compilation")
-st.write(
-    "Mari Kita lihat kompilasi muka shandi"
+st.title("Kontroversi Nafathan")
+st.write  
+"Salah Satu Anggota De Golden Mendukung Aksi Pembubaran"
 )
 st.image("IMG_4612.png", width=400
