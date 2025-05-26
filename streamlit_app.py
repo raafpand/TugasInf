a31e45f895efc56ed5cfc979f86fe5348277b724
raafpand <raafipandhika@gmail.com>
2025-05-27 06:59:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Kontroversi Nafathan")
 
-st.write("Salah Satu Anggota De Golden Mendukung Aksi Pembubaran")
-
-st.image("cIMG_4612.png",width=400")
+st.write("Salah Satu Anggota De Golden Mendukung Aksi Pembubaran"
+        )
+st.image("cIMG_4612.png", width=200")
